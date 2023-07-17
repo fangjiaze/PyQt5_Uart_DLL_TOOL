fe84403f2c8f6e23a5395bc48fb35a58d4462cc3
fangjiaze <fjz@lechang-iot.com>
2023-07-17 11:42:14 +0800

    优化了按键连接方式，这样的方式可控范围更大

--- cp05_protocol_tool_form.py
+++ cp05_protocol_tool_form.py
@@ -10,13 +10,13 @@ class cp05_protocol_tool_form(QtWidgets.QWidget,Ui_Form) :
 
         self.json_str = """{
             "map":[
-                {"n":2,"param1":"string1data1","param2":"string1data2","button":"ctr1"},
-                {"n":1,"param1":"string2data1","button":"ctr2"},
-                {"n":3,"param1":"string3data1","param2":"string3data2","param3":"string3data3","button":"ctr3"}
+                {"n":2,"param1":"string1data1","param2":"string1data2","button":"ctr1","callback":"abc"},
+                {"n":1,"param1":"string2data1","button":"ctr2","callback":"abc"},
+                {"n":3,"param1":"string3data1","param2":"string3data2","param3":"string3data3","button":"ctr3","callback":"abc"}
             ]
         }"""
 
-        self.line_edit_map = {}
+        self.line_edit_map = []
 
         json_obj = json.loads(self.json_str)
 
@@ -45,9 +45,11 @@ class cp05_protocol_tool_form(QtWidgets.QWidget,Ui_Form) :
             button = QPushButton(obj["button"])
             self.gridLayout.addWidget(button, i, obj["n"] * 2)
 
-            self.line_edit_map[obj["button"]] = line_edit_list
+            # self.line_edit_map[obj["button"]] = line_edit_list
+            self.line_edit_map.append(line_edit_list)
 
-            button.clicked.connect(lambda checked, text=obj["button"]: self.callback(text))
+            # button.clicked.connect(lambda checked, text=obj["button"]: self.callback(text))
+            button.clicked.connect(lambda checked, index=i: self.callback(index))
 
         # self.widget.setLayout(self.layout)
         # self.widget.show()
@@ -57,15 +59,15 @@ class cp05_protocol_tool_form(QtWidgets.QWidget,Ui_Form) :
         if "map" not in json_obj:
             return False
         for obj in json_obj["map"]:
-            if "n" not in obj or "button" not in obj:
+            if "n" not in obj or "button" not in obj or "callback" not in obj:
                 return False
             for i in range(1, obj["n"] + 1):
                 if f"param{i}" not in obj:
                     return False
         return True
 
-    def callback(self, button_text):
+    def callback(self, index):
         # global line_edit_map
-        line_edits = self.line_edit_map[button_text]
+        line_edits = self.line_edit_map[index]
         for line_edit in line_edits:
             print(line_edit.text())
